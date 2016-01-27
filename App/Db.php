26789ad97988074efe2b5809221ee74fe4746c8a
bigazzzz <bigazzzz@gmail.com>
2016-01-27 22:42:35 +0300

    + добавлене getWhereDefinition в класс Db ! добавлена обработка подстановок в query и execute

--- App/Db.php
+++ App/Db.php
@@ -13,21 +13,34 @@ class Db
         $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
     }
 
-    public function execute($sql)
+    public function execute($sql, $substitutions = array())
     {
-        $sth = $this->dbh->prepare($sql);
-        $res = $sth->execute();
+        $sth = $this->dbh->prepare($sql . $this->getWhereDefinition($substitutions), $substitutions);
+        $res = $sth->execute($substitutions);
         return $res;
     }
 
-    public function query($sql, $class)
+    public function query($sql, $class, $substitutions = array())
     {
-        $sth = $this->dbh->prepare($sql);
-        $res = $sth->execute();
+        $sth = $this->dbh->prepare($sql . $this->getWhereDefinition($substitutions), $substitutions);
+        $res = $sth->execute($substitutions);
         if (false !== $res) {
             return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
         }
         return [];
+    }
+
+    private function getWhereDefinition($substitutions = array())
+    {
+        $where_definition = '';
+        if ($substitutions){
+            $where_definition = ' WHERE ';
+            foreach ($substitutions as $row=>$value) {
+                $where_definition .= $row . '=:' . $row . " AND ";
+            }
+            $where_definition = substr($where_definition, 0, -5);
+        }
+        return $where_definition;
     }
 
 }
