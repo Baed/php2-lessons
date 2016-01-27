702425e6aed3b6e7716a3cfe2ced36532853896f
bigazzzz <bigazzzz@gmail.com>
2016-01-28 01:05:36 +0300

    * исправлены тесты

--- tests/test1.php
+++ tests/test1.php
@@ -4,5 +4,13 @@ require __DIR__ . '/../autoload.php';
 require __DIR__ . '/index.php';
 
 $db = new \App\Db();
-$users = $db->query('SELECT * FROM users', '\App\Models\User', $_REQUEST);
+$where_definition = '';
+    if ($_REQUEST){
+        $where_definition = ' WHERE ';
+        foreach ($_REQUEST as $row=>$value) {
+            $where_definition .= $row . '=:' . $row . " AND ";
+        }
+        $where_definition = substr($where_definition, 0, -5);
+    }
+$users = $db->query('SELECT * FROM users' . $where_definition , '\App\Models\User', $_REQUEST);
 var_dump($users);
