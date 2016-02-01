c412c6c75fa15ffbb4728139ad1f2ec2bec509a3
bigazzzz <bigazzzz@gmail.com>
2016-02-02 01:13:40 +0300

    #L2T6 +add delete +add edit

---
+++ App/Views/admin_edit.html
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>Создать новость</h1>
+<form action="admin.php" method="post">
+    <input type="hidden" name="op" value="save" />
+    <input type="hidden" name="id" value="<?php echo $objects->id; ?>" />
+    <p>Название <input type="text" name="title" value="<?php echo $objects->title; ?>" /></p>
+    <p>Вступление<textarea name="intro_text"><?php echo $objects->intro_text; ?></textarea></p>
+    <p>Продолжение<textarea name="full_text"><?php echo $objects->full_text; ?></textarea></p>
+    <p><input type="submit" value="Отправить" /> </p>
+</form>
+
+</body>
+</html>
--- admin.php
+++ admin.php
@@ -9,11 +9,30 @@ switch($op){
         $article = new \App\Models\News();
         $article->title = $_REQUEST['title'];
         $article->intro_text = $_REQUEST['intro_text'];
-        $article->full_text = $_REQUEST['intro_text'];
+        $article->full_text = $_REQUEST['full_text'];
         $article->author = 1;
         $article->created_at = date('Y-m-d H:i:s');
         $article->save();
         $html = new \App\View('admin_redirect', array('message'=>'Новость добавлена'));
+        break;
+
+    case 'save':
+        $article = \App\Models\News::findById($_REQUEST['id']);
+        $article->title = $_REQUEST['title'];
+        $article->intro_text = $_REQUEST['intro_text'];
+        $article->full_text = $_REQUEST['full_text'];
+        $article->save();
+        $html = new \App\View('admin_redirect', array('message'=>'Новость изменена'));
+        break;
+
+    case 'delete':
+        \App\Models\News::findById($_REQUEST['id'])->delete();
+        $html = new \App\View('admin_redirect', array('message'=>'Новость удалена'));
+        break;
+
+    case 'edit':
+        $article = \App\Models\News::findById($_REQUEST['id']);
+        $html = new \App\View('admin_edit', $article);
         break;
 
     default:
