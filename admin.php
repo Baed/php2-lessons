6d613557641dff4deb33f4fabe657236aa69b0ec
bigazzzz <bigazzzz@gmail.com>
2016-02-02 00:52:30 +0300

    #L2T6 + первая страница админки

--- admin.php
+++ admin.php
@@ -4,6 +4,11 @@ require __DIR__ . '/autoload.php';
 
 $op = $_REQUEST['op'];
 switch($op){
+
+    case 'add':
+        var_dump($_REQUEST);
+        break;
+
     default:
         $news = \App\Models\News::findAll();
         $html = new \App\View('admin_main', $news);
