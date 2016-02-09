8b4e1ec396676c59e2dd23030662c2d21f0f14d9
socnetspam@gmail.com <bigazzzz@gmail.com>
2016-02-09 16:48:15 +0300

    #L3T2. Выполнено

--- App/Models/News.php
+++ App/Models/News.php
@@ -6,13 +6,26 @@ use App\Model;
 
 class News extends Model
 {
+
     const TABLE = 'articles';
 
-    public $author;
+    public $author_id;
     public $title;
     public $intro_text;
     public $full_text;
     public $created_at;
     public $modifyed_at;
+
+    public function __get($k)
+    {	
+    	if ($k == 'author'){
+	    	if (isset($this->author_id)){
+	    		return \App\Models\Author::findById($this->author_id);
+	    	} else {
+	    		return FALSE;
+	    	}
+    	}
+    	return NULL;
+    }
 
 }
