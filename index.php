116505620231861ca91427f4fcdce690f8863475
socnetspam@gmail.com <bigazzzz@gmail.com>
2016-02-12 13:10:07 +0300

    #L4T3 Выполнено

--- index.php
+++ index.php
@@ -2,14 +2,27 @@
 
 require __DIR__ . '/autoload.php';
 
-$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-$path_array = explode('/',$path);
-$controller_path = '\\App\\Controllers\\' . implode('\\', $path_array);
 $action = "Index";
-if (!class_exists($controller_path)){
-	$action = array_pop($path_array);
+$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+if ($path == 'index.php'){
+	$controller_path = '\\App\\Controllers\\' . $_GET['ctrl'];
+	$action = isset($_GET['action'])?$_GET['action']:"Index";
+	if (!class_exists($controller_path)){
+			header('HTTP/1.0 404 not found');
+			echo "404";
+			die();
+	}
+} else {
+	$path_array = explode('/',$path);
 	$controller_path = '\\App\\Controllers\\' . implode('\\', $path_array);
 	if (!class_exists($controller_path)){
+		$action = array_pop($path_array);
+		$controller_path = '\\App\\Controllers\\' . implode('\\', $path_array);
+		if (!class_exists($controller_path)){
+			header('HTTP/1.0 404 not found');
+			echo "404";
+			die();
+		}
 	}
 }
 $controller = new $controller_path();
